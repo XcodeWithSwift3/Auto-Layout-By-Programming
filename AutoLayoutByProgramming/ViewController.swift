cb4b69b58ee6730d8814a4b805783bbb75faa4ff
Lê Hồng Phong <lehongphong@Les-Mac.local>
2017-02-27 10:31:21 +0700

    Initial Commit

---
+++ AutoLayoutByProgramming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutByProgramming
+//
+//  Created by Lê Hồng Phong on 2/27/17.
+//  Copyright © 2017 Lê Hồng Phong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
